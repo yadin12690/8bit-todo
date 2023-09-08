d5a276253727d98fce82e335c8016d038126e971
Yadin Yafe <yadin258@yahoo.com>
2023-09-08 19:37:10 +0300

    change ing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,9 @@
 'use client'
 
-
-import React, { ChangeEvent, useRef, useState } from 'react'
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import cardImage from '../assets/task-card.png';
 import Image from 'next/image'
 import { dotGothic16, press_start_2P } from '../../../fonts'
-
-
-
 
 export default function Dashboard() {
     const [inputValue, setInputValue] = useState('');
@@ -24,10 +20,11 @@ export default function Dashboard() {
             <div className="card-container flex justify-end">
                 <Image src={cardImage} alt="lofi-todo" width={500} height={500} />
                 <div className="content-container flex justify-center items-center flex-col relative gap-2 top-40 right-[26rem] h-fit">
-                    <div className={`${press_start_2P.className} pixel-text-area text-xs lvl2 text-gray-500`}><input ref={userTask} placeholder="Enter task content..." /></div>
-                    <div className="button-container">
-                        <button className={`${press_start_2P.className} addNewTaskBtn text-black`}>Add new task</button>
-                    </div>
+                    <div className={`${press_start_2P.className} pixel-text-area text-xs lvl2 text-gray-500`}><input ref={userTask}
+                        onChange={addNewTask}
+                        placeholder="Enter task content..." /></div>
+                    <button className={`${press_start_2P.className} addNewTaskBtn text-black`} onClick={() => console.log(userTask.current?.value)
+                    }>Add new task</button>
                 </div>
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
-import React, { useRef } from 'react'
-import cardImage from '../app/assets/task-card.png';
-import Image from 'next/image'
-import { dotGothic16, press_start_2P } from '../../fonts'
-<link rel="icon" href="/favicon8.png" sizes="any" />
+import React from 'react'
+import { dotGothic16 } from '../../fonts'
 
 export default function Home() {
 
   return (
     <div className='flex justify-end flex-col w-full h-full py-20 px-20'>
-      <h1 className={`${dotGothic16.className} text-white h-fit font-bold text-8xl text-center uppercase`}>Tasks dashboard</h1>
-      <div className="card-container flex justify-end">
-        <Image src={cardImage} alt="lofi-todo" width={500} height={500} />
-        <div className="content-container flex justify-center items-center flex-col relative gap-2 top-40 right-[26rem] h-fit">
-          <div className={`${press_start_2P.className} pixel-text-area text-xs lvl2 text-gray-500`}><span >Enter task content...</span></div>
-          <div className="button-container">
-            <button className={`${press_start_2P.className} addNewTaskBtn text-black`}>Add new task</button>
-          </div>
-        </div>
-      </div>
+      <h1 className={`${dotGothic16.className} text-white h-fit font-bold text-8xl text-center uppercase`}>Welcome Abored</h1>
     </div>
   )
 }
