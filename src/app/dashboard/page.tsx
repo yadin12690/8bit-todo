8412d49df44d66f5ca436b61c1b8d0eacc5ce398
Yadin Yafe <yadin258@yahoo.com>
2023-09-08 20:13:40 +0300

    add more

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,13 +6,32 @@ import Image from 'next/image'
 import { dotGothic16, press_start_2P } from '../../../fonts'
 
 export default function Dashboard() {
-    const [inputValue, setInputValue] = useState('');
-    const userTask = useRef<HTMLInputElement | null>(null);
 
-    const addNewTask = (e: ChangeEvent<HTMLInputElement>) => {
-        const newValue = e.target.value;
-        setInputValue(newValue);
+    const [newTask, setNewTask] = useState('Enter task content...');
+
+    const divRef = useRef<HTMLDivElement | null>(null);
+
+    // const addNewTask = (e: ChangeEvent<HTMLInputElement>) => {
+    //     const newValue = e.target.value;
+    //     setInputValue(newValue);
+    // };
+
+    const handleBlur = () => {
+        if (divRef.current) {
+            divRef.current.innerText = ""
+            setNewTask(divRef.current.innerText);
+        }
+    }
+
+    const handleClick = () => {
+        if (divRef.current && divRef.current.innerText === 'Enter task content...') {
+            // Clear the content when clicked if it's the default text
+            divRef.current.innerText = '';
+        }
     };
+
+
+
 
     return (
         <div className='flex justify-end flex-col w-full h-full py-20 px-20'>
@@ -20,10 +39,14 @@ export default function Dashboard() {
             <div className="card-container flex justify-end">
                 <Image src={cardImage} alt="lofi-todo" width={500} height={500} />
                 <div className="content-container flex justify-center items-center flex-col relative gap-2 top-40 right-[26rem] h-fit">
-                    <div className={`${press_start_2P.className} pixel-text-area text-xs lvl2 text-gray-500`}><input ref={userTask}
-                        onChange={addNewTask}
-                        placeholder="Enter task content..." /></div>
-                    <button className={`${press_start_2P.className} addNewTaskBtn text-black`} onClick={() => console.log(userTask.current?.value)
+                    <div
+                        ref={divRef}
+                        onClick={handleClick}
+                        suppressContentEditableWarning={true}
+                        onBlur={handleBlur}
+                        contentEditable={true}
+                        className={`${press_start_2P.className} pixel-text-area text-xs lvl2 text-gray-500 overflow-auto min-w-[19rem] max-w-[19rem] min-h-[7rem] max-h-[7rem]`}>{newTask}</div>
+                    <button className={`${press_start_2P.className} addNewTaskBtn text-black`} onClick={() => console.log(newTask)
                     }>Add new task</button>
                 </div>
             </div>
